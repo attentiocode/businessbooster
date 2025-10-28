c1ca764a06e056357e34e77d832e64b4a81e9dd2
Kai Bertelsen <kai@exposoft.no>
2025-10-28 14:04:37 +0100

    ny

--- select.js
+++ select.js
@@ -603,7 +603,7 @@ function updateSelectCounterDark(
 
   const totalEl  = makeChip('Totalt', sumTotal, colors.total);
   const portalEl = makeChip('I portal', sumPortal, colors.portal);
-  const inProcessEl = makeChip('I epostløp', sumInProsess, colors.Prosess);
+  const inProcessEl = makeChip('I epostløp', sumInProsess, colors.prosess);
   const readyEl  = makeChip('Klar', sumReady, colors.ready);
   const utvalgEl = makeChip('I utvalg', sumUtvalg, colors.utvalg);
   const emailEl  = makeChip('Har e-post', sumEmail, colors.email);
