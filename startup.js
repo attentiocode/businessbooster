7435a1aff87dc0b7d1f5c62c447b7e6b2428e9c0
Kai Bertelsen <kai@exposoft.no>
2025-11-07 10:37:21 +0100

    ny

--- startup.js
+++ startup.js
@@ -246,7 +246,7 @@ document.getElementById("brregmastercheckbox").addEventListener("change", functi
   //hvis det er mer en 1 checkbox som er valgt så gjør maassbehandling synlig
   const bulkBar   = document.getElementById('select-bulk-actions-brreg');
   const checkboxesChecked = container.querySelectorAll(".selectcheckbox:checked");
-  if (checkboxesChecked.length > 0){
+  if (!this.checked){
     bulkBar.style.display = "flex";
     const bulkCount = document.getElementById('select-bulk-count-brreg');
     //finne ut hvor mange chackboxer som er checked i listen med id rowlistSelect
