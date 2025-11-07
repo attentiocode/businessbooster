1cd10956411cba76f903a0438261bfb73d73361f
Kai Bertelsen <kai@exposoft.no>
2025-11-07 11:18:24 +0100

    ny

--- startup.js
+++ startup.js
@@ -85,7 +85,31 @@ document.getElementById("xls-download-select").addEventListener("click", functio
 });
 
 function generateAndDownloadXls(filename,data){
-  exportXLS(data, filename)
+
+
+
+  let testdata = [
+    {"Organisasjonsnummer": "123456789",
+    "Navn": "Test AS",
+    "Adresse": "Testveien 1",
+    "Postnummer": "0123",
+    "Poststed": "Oslo"
+    },
+    {"Organisasjonsnummer": "987654321",
+    "Navn": "Eksempel AS",
+    "Adresse": "Eksempelgata 2",
+    "Postnummer": "0456",
+    "Poststed": "Bergen"
+    }];
+
+
+
+
+
+
+
+
+  exportXLS(testdata, filename);
 }
 
 
